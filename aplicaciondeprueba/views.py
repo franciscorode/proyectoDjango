707fb76d3584b05698dd59027068297fc2b0ceca
Francisco Rode <fran_rode@hotmail.com>
2017-07-29 21:59:35 +0200

    commit plantillas django y datos dinamicos

--- aplicaciondeprueba/views.py
+++ aplicaciondeprueba/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 #hemos creado un método (def) llamado post_list que toma un request y hace un return de un método 
 #render que renderizará (construirá) nuestra plantilla blog/post_list.html
 def post_list(request):
-    return render(request, 'aplicaciondeprueba/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'aplicaciondeprueba/post_list.html', {'posts': posts})
